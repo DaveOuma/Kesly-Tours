fd4b18209aebf2ef2137e072e078b89c805eb6cb
David Ouma <davecarlosotieno@gmail.com>
2026-04-01 15:01:50 +0300

    Prepare project for Vercel deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6uhbubv835kti4-3vrs7dn2!b0g_8g5c^@xou9q+e$ae)$yd*(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app"]
 
 
 # Application definition
